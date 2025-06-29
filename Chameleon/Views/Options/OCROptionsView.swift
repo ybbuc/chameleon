29c77cd3c237862c9030225997b4c7a91ef43430
Jakob Wells <jakob@duck.com>
2025-06-29 21:03:25 +0200

    style: fix indentation in OCROptionsView

--- Chameleon/Views/Options/OCROptionsView.swift
+++ Chameleon/Views/Options/OCROptionsView.swift
@@ -26,41 +26,41 @@ struct OCROptionsView: View {
         }
     
     var body: some View {
-            Form {
-                Picker("Quality:", selection: $ocrOptions.recognitionLevel) {
-                    ForEach(OCRService.RecognitionLevel.allCases, id: \.self) { level in
-                        Text(level.rawValue).tag(level)
-                    }
+        Form {
+            Picker("Quality:", selection: $ocrOptions.recognitionLevel) {
+                ForEach(OCRService.RecognitionLevel.allCases, id: \.self) { level in
+                    Text(level.rawValue).tag(level)
                 }
-                .pickerStyle(.segmented)
-                .onChange(of: ocrOptions.recognitionLevel) { _, newLevel in
-                    // Disable language correction when switching to Fast
-                    if newLevel == .fast {
-                        ocrOptions.usesLanguageCorrection = false
-                    }
-                }
-                .transition(.opacity.combined(with: .move(edge: .top)))
-                
-                Picker("Language:", selection: languageSelectionBinding) {
-                    // Automatic option
-                    Text("Automatic").tag("automatic")
-                    
-                    Divider()
-                    
-                    // Other language options
-                    ForEach(OCRService.Language.supportedLanguages.filter { $0.id != "automatic" }, id: \.id) { language in
-                        Text(language.displayName).tag(language.id)
-                    }
-                }
-                .pickerStyle(.menu)
-                .onChange(of: ocrSelectedLanguage) { _, newValue in
-                    ocrOptions.recognitionLanguages = [newValue]
-                }
-                .transition(.opacity.combined(with: .move(edge: .top)))
-                
-                Toggle("Language Correction", isOn: $ocrOptions.usesLanguageCorrection)
-                    .disabled(ocrOptions.recognitionLevel == .fast)
-                    .transition(.opacity.combined(with: .move(edge: .top)))
             }
+            .pickerStyle(.segmented)
+            .onChange(of: ocrOptions.recognitionLevel) { _, newLevel in
+                // Disable language correction when switching to Fast
+                if newLevel == .fast {
+                    ocrOptions.usesLanguageCorrection = false
+                }
+            }
+            .transition(.opacity.combined(with: .move(edge: .top)))
+            
+            Picker("Language:", selection: languageSelectionBinding) {
+                // Automatic option
+                Text("Automatic").tag("automatic")
+                
+                Divider()
+                
+                // Other language options
+                ForEach(OCRService.Language.supportedLanguages.filter { $0.id != "automatic" }, id: \.id) { language in
+                    Text(language.displayName).tag(language.id)
+                }
+            }
+            .pickerStyle(.menu)
+            .onChange(of: ocrSelectedLanguage) { _, newValue in
+                ocrOptions.recognitionLanguages = [newValue]
+            }
+            .transition(.opacity.combined(with: .move(edge: .top)))
+            
+            Toggle("Language Correction", isOn: $ocrOptions.usesLanguageCorrection)
+                .disabled(ocrOptions.recognitionLevel == .fast)
+                .transition(.opacity.combined(with: .move(edge: .top)))
+        }
     }
 }
